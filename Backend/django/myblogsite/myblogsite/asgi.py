0ffba5f8bd5633d09f58d92ad0153fcab6a04988
maurya-goyal10 <mauryagoyal@gmail.com>
2023-07-04 13:02:18 +0530

    made the models file

--- Backend/django/myblogsite/myblogsite/asgi.py
+++ Backend/django/myblogsite/myblogsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for clone_sites project.
+ASGI config for myblogsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clone_sites.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblogsite.settings')
 
 application = get_asgi_application()
--- Backend/django/myblogsite/myblogsite/wsgi.py
+++ Backend/django/myblogsite/myblogsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for clone_sites project.
+WSGI config for myblogsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clone_sites.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblogsite.settings')
 
 application = get_wsgi_application()
