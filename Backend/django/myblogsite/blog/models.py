ae55976c039cf1233690d21946428254eed21b28
maurya-goyal10 <mauryagoyal@gmail.com>
2023-07-05 10:43:36 +0530

    made all the views and added to the urls file

--- Backend/django/myblogsite/blog/models.py
+++ Backend/django/myblogsite/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True,null=True)
     
     def publish(self):
@@ -27,7 +27,7 @@ class Comment(models.Model):
     post = models.ForeignKey('blog.Post',on_delete=models.CASCADE,related_name='comments')
     author = models.CharField(max_length=512)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
     
     def approve(self):
--- Backend/django/myblogsite/blog/views.py
+++ Backend/django/myblogsite/blog/views.py
@@ -1,3 +1,50 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from blog.models import Post,Comment
+from blog import forms
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (TemplateView,ListView,DetailView,
+                                  CreateView,UpdateView,DeleteView)
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+    
+class PostListView(ListView):
+    model = Post
+    
+    def get_queryset(self):
+        # - in order by indicates decreasing order
+        return Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = forms.PostForm
+    model = Post
+    
+class UpdatePostView(LoginRequiredMixin,UpdateView):
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = forms.PostForm
+    model = Post
+    
+class DeletePostView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog.post_list')
+    
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+    
+    def get_queryset(self):
+        return Post.objects.filter(publish_date_isnull=True).order_by('-create_date')
+    
+    
