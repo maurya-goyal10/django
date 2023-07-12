7ec57b85df19792a85ad80640d2a7a7b07c5de68
maurya-goyal10 <mauryagoyal@gmail.com>
2023-07-12 11:20:04 +0530

    created posts views and urls

---
+++ simple_social/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.PostList.as_view(),name='all'),
+    path('new/',views.CreatePost.as_view(),name='create'),
+    path('by/<str:username>/',views.UserPost.as_view(),name='for_user'),
+    path('by/<str:username>/<int:pk>/',views.PostDetail.as_view(),name='single'),
+    path('delete/<int:pk>/',views.DeletePost.as_view(),name='delete')
+]
