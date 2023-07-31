b9b7aad9949f574f06929095de897d4cba4b1da2
maurya-goyal10 <mauryagoyal@gmail.com>
2023-07-31 16:04:34 +0530

    making changes to the admin portal

--- Backend/django/my_video_rental/my_video_rental/settings.py
+++ Backend/django/my_video_rental/my_video_rental/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'my_video_rental.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Backend/django/my_video_rental/videos/models.py
+++ Backend/django/my_video_rental/videos/models.py
@@ -6,10 +6,15 @@ class Movie(models.Model):
     length = models.PositiveIntegerField()
     release_year = models.PositiveIntegerField()
     
+    def __str__(self):
+        return self.name
+    
 class Customer(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     phone = models.PositiveIntegerField()
     
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
     
