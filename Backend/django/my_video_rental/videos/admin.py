72aee66f641e08e2c1af1f71020845658a4b4828
maurya-goyal10 <mauryagoyal@gmail.com>
2023-08-01 11:39:34 +0530

    completed the course

--- Backend/django/my_video_rental/videos/admin.py
+++ Backend/django/my_video_rental/videos/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-admin.site.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    
+    fields = ['release_year','name','length']
+    search_fields = ['name','release_year']
+    list_filter = ['release_year','length']
+    list_display = ['name','release_year','length']
+
+admin.site.register(models.Movie,MovieAdmin)
 admin.site.register(models.Customer)
